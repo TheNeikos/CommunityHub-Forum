f54730c9018a520967e7d77d0276d995f7642614
Neikos <com2040@gmail.com>
2014-03-10 20:47:16 +0100

    Added some more user functions and requires

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var db = require("./lib/database");
 
 var app = express();
 
+require("./lib/user")(app);
 require('./lib/modules')(app);
-
 
 var server = app.listen(3000, function () {
     console.log("Listening on port %d", server.address().port);
